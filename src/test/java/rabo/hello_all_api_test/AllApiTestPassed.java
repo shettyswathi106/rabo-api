de807a3ffb437941441332885e69fdba69f3b279
unknown <harshad.rayunipet@gmail.com>
2020-08-30 13:18:06 +0200

    rabo

---
+++ src/test/java/rabo/hello_all_api_test/AllApiTestPassed.java
@@ -0,0 +1,18 @@
+package rabo.hello_all_api_test;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class AllApiTestPassed {
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String sayHello() {
+	return "“Hello, all API tests were successful'";
+	}
+}
+
+
---
+++ src/test/java/rabo/hello_all_api_test/SpringBootApplicationLuncher.java
@@ -0,0 +1,9 @@
+package rabo.hello_all_api_test;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringBootApplicationLuncher {
+	public static void main(String[] args) {
+		SpringApplication.run(AllApiTestPassed.class, args);
+		}
+}
